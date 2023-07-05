4be6660ccec5d3a828b3d40fd714cda3f9d60878
sungseanpark <sungseanpark@gmail.com>
2023-07-04 19:14:14 -0700

    Add score displayer

--- game.js
+++ game.js
@@ -28,10 +28,16 @@ function playRound(playerSelection, computerSelection) {
                 case "paper":
                     console.log("You Lose! Paper Beats Rock");
                     result.textContent = "You Lose! Paper Beats Rock";
+                    computerScore++;
+                    const computerScoreDiv = document.querySelector('.score-computer');
+                    computerScoreDiv.textContent = 'Computer: ' + computerScore;
                     return "computer";
                 case "scissors":
                     console.log("You Win! Rock Beats Scissors");
                     result.textContent = "You Win! Rock Beats Scissors";
+                    playerScore++;
+                    const playerScoreDiv = document.querySelector('.score-player');
+                    playerScoreDiv.textContent = 'Player: ' + playerScore;
                     return "player";
             }
             break;
@@ -40,6 +46,9 @@ function playRound(playerSelection, computerSelection) {
                 case "rock":
                     console.log("You Win! Paper Beats Rock");
                     result.textContent = "You Win! Paper Beats Rock";
+                    playerScore++;
+                    const playerScoreDiv = document.querySelector('.score-player');
+                    playerScoreDiv.textContent = 'Player: ' + playerScore;
                     return "player";
                 case "paper":
                     console.log("You Tie! Paper Ties Paper");
@@ -48,6 +57,9 @@ function playRound(playerSelection, computerSelection) {
                 case "scissors":
                     console.log("You Lose! Scissors Beats Paper");
                     result.textContent = "You Lose! Scissors Beats Paper";
+                    computerScore++;
+                    const computerScoreDiv = document.querySelector('.score-computer');
+                    computerScoreDiv.textContent = 'Computer: ' + computerScore;
                     return "computer";
             }
             break;
@@ -56,10 +68,16 @@ function playRound(playerSelection, computerSelection) {
                 case "rock":
                     console.log("You Lose! Rock Beats Scissors");
                     result.textContent = "You Lose! Rock Beats Scissors";
+                    computerScore++;
+                    const computerScoreDiv = document.querySelector('.score-computer');
+                    computerScoreDiv.textContent = 'Computer: ' + computerScore;
                     return "computer";
                 case "paper":
                     console.log("You Win! Scissors Beats Paper");
                     result.textContent = "You Win! Scissors Beats Paper";
+                    playerScore++;
+                    const playerScoreDiv = document.querySelector('.score-player');
+                    playerScoreDiv.textContent = 'Player: ' + playerScore;
                     return "player";
                 case "scissors":
                     console.log("You Tie! Scissors Ties Scissors");
